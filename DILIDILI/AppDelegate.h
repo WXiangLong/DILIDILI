5bf60f5cc66a83536ce179ff215662cf8d968172
WXiangLong <942519324@qq.com>
2016-04-22 10:40:09 +0800

    Initial Commit

---
+++ DILIDILI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DILIDILI
+//
+//  Created by LONG on 16/4/22.
+//  Copyright © 2016年 LONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DILIDILI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DILIDILI
+//
+//  Created by LONG on 16/4/22.
+//  Copyright © 2016年 LONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
