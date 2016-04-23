7becff2093eb0fe13ece54e1ff0e710140dbf246
WXiangLong <942519324@qq.com>
2016-04-23 10:28:34 +0800

    完成翻页和动画

--- DILIDILI/Classes/Detail/DetailViewViewController.m
+++ DILIDILI/Classes/Detail/DetailViewViewController.m
@@ -17,6 +17,9 @@
 
 @property (nonatomic) UIImageView * currentImageView;
 
+
+@property (nonatomic) UIImageView * playImageView;
+
 @property (nonatomic) UILabel * titleLabel;
 
 @property (nonatomic) UIView * line;
@@ -24,6 +27,20 @@
 @property (nonatomic) UILabel * categryAndTimeLabel;
 
 @property (nonatomic) UILabel * descriptionLabel;
+
+
+@property (nonatomic) NSString * descriptionString;
+
+@property (nonatomic) NSString * categryAndTimeString;
+
+@property (nonatomic) NSString * titleString;
+
+
+@property (nonatomic) NSThread * descriptionThread;
+
+@property (nonatomic) NSThread * categryAndTimeThread;
+
+@property (nonatomic) NSThread * titleThread;
 
 @end
 
@@ -33,29 +50,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
 }
-
-- (void) createCurrentViewModel:(HotRankingModel *)model
-{
-    // 上滑返回，删除view
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(removeView) name:@"ChangeTableViewOffset" object:nil];
-    // 滚动，改变透明度
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeAlpha:) name:@"scrollBegan" object:nil];
-    
-    // 滚动结束，透明度为1，改变UI
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateUI:) name:@"scrollEnd" object:nil];
-    
-    
-    
-    _rootView = [[UIView alloc] initWithFrame:CGRectMake(0, SH - SH / 2 + 64, SW, SH / 2 + 64)];
-    
-    _rootView.userInteractionEnabled = NO;
-
-    AppDelegate * app = [UIApplication sharedApplication].delegate;
-    
-    [app.window.rootViewController.view addSubview:_rootView];
-    
-    [self createUI:model];
-}
+#pragma mark - 创建下面一层View
 - (void) createNextViewModel:(HotRankingModel *)model
 {
     // cell将要出现时候改变nextimageView的图片
@@ -76,10 +71,45 @@
 {
     _nextImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, SW, _nextView.frame.size.height)];
     
-        [_nextImageView sd_setImageWithURL:model.cover[@"blurred"]];
+    [_nextImageView sd_setImageWithURL:model.cover[@"blurred"]];
     
     [_nextView addSubview:_nextImageView];
 }
+
+#pragma mark - 创建上面一层View
+- (void) createCurrentViewModel:(HotRankingModel *)model
+{
+    // 上滑返回，删除view
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(removeView) name:@"ChangeTableViewOffset" object:nil];
+    // 滚动，改变透明度
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(changeAlpha:) name:@"scrollBegan" object:nil];
+    
+    // 滚动结束，透明度为1，改变UI
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updateUI:) name:@"scrollEnd" object:nil];
+    
+    
+    _playImageView = [[UIImageView alloc] initWithFrame:CGRectMake(SW/2-50, 64+SH/4-50, 100, 100)];
+    
+    _playImageView.userInteractionEnabled = NO;
+    
+    _playImageView.backgroundColor = [UIColor redColor];
+    
+    AppDelegate * app = [UIApplication sharedApplication].delegate;
+    
+    [app.window.rootViewController.view addSubview:_playImageView];
+    
+    
+    _rootView = [[UIView alloc] initWithFrame:CGRectMake(0, SH - SH / 2 + 64, SW, SH / 2 + 64)];
+    
+    _rootView.userInteractionEnabled = NO;
+
+    AppDelegate * app1 = [UIApplication sharedApplication].delegate;
+    
+    [app1.window.rootViewController.view addSubview:_rootView];
+    
+    [self createUI:model];
+}
+
 
 - (void) createUI:(HotRankingModel *)model
 {
@@ -97,13 +127,17 @@
     
     _titleLabel.textColor = [UIColor whiteColor];
     
-    _titleLabel.text = model.title;
+    _titleString = model.title;
+    
+    _titleThread = [[NSThread alloc] initWithTarget:self selector:@selector(animationLabel) object:nil];
+    
+    [_titleThread start];
     
     [_rootView addSubview:_titleLabel];
     
     
     
-    CGSize contentSize1 = [self contentSizeOfString:_titleLabel.text maxWidth:SW-20 font:[UIFont systemFontOfSize:20]];
+    CGSize contentSize1 = [self contentSizeOfString:model.title maxWidth:SW-20 font:[UIFont systemFontOfSize:20]];
     
     _line = [[UIView alloc] initWithFrame:CGRectMake(10, _titleLabel.frame.origin.y + _titleLabel.frame.size.height + _rootView.frame.size.height * 0.03, contentSize1.width * 0.8, 1)];
     
@@ -120,25 +154,31 @@
     
     _categryAndTimeLabel.textColor = [UIColor whiteColor];
     
-    _categryAndTimeLabel.text = [NSString stringWithFormat:@"#%@ / %02ld'%02ld''",model.category,model.duration/60,model.duration%60];
+    _categryAndTimeString = [NSString stringWithFormat:@"#%@ / %02ld'%02ld''",model.category,model.duration/60,model.duration%60];
+    
+    _categryAndTimeThread = [[NSThread alloc] initWithTarget:self selector:@selector(animationLabel1) object:nil];
+    
+    [_categryAndTimeThread start];
     
     [_rootView addSubview:_categryAndTimeLabel];
     
     
-    
-    CGSize contentSize = [self contentSizeOfString:model.myDescription maxWidth:SW-20 font:[UIFont systemFontOfSize:15]];
-    
-    _descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, _categryAndTimeLabel.frame.origin.y + _categryAndTimeLabel.frame.size.height + _rootView.frame.size.height * 0.03, SW - 20, contentSize.height)];
+    _descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, _categryAndTimeLabel.frame.origin.y + _categryAndTimeLabel.frame.size.height + _rootView.frame.size.height * 0.03, SW - 20, 0)];
     
     _descriptionLabel.font = [UIFont systemFontOfSize:15];
     
     _descriptionLabel.textColor = [UIColor whiteColor];
     
     _descriptionLabel.numberOfLines = 0;
+
+    _descriptionString = model.myDescription;
     
-    _descriptionLabel.text = model.myDescription;
+    _descriptionThread = [[NSThread alloc] initWithTarget:self selector:@selector(animationLabel2) object:nil];
+    
+    [_descriptionThread start];
     
     [_rootView addSubview:_descriptionLabel];
+    
 #warning 收藏下载
 }
 // 计算label高度
@@ -155,6 +195,8 @@
     [_rootView removeFromSuperview];
     
     [_nextView removeFromSuperview];
+    
+    [_playImageView removeFromSuperview];
 }
 
 - (void) changeAlpha:(NSNotification *)info
@@ -164,6 +206,18 @@
     float oldX = [info.object[@"oldX"] floatValue];
     
     _rootView.alpha = 1 - fabsf(newX - oldX)/ SW;
+    
+    if (newX != oldX)
+    {
+        if ( 0 < fabsf(newX - oldX)  &&  fabsf(newX - oldX) < SW / 2)
+        {
+            _playImageView.alpha = 1 - fabsf(newX - oldX)/ (SW / 2);
+        }
+        else
+        {
+            _playImageView.alpha = fabsf(newX - oldX)/ (SW / 2) - 1;
+        }
+    }
 }
 
 - (void) changeNextImageView:(NSNotification *)info
@@ -175,13 +229,31 @@
 
 - (void) updateUI:(NSNotification *)info
 {
+    
+//    if (!(_titleThread.isCancelled))
+//    {
+//        [_titleThread cancel];
+//    }
+//    if (!(_categryAndTimeThread.isCancelled))
+//    {
+//        [_categryAndTimeThread cancel];
+//    }
+//    if (!(_descriptionThread.isCancelled))
+//    {
+//        [_descriptionThread cancel];
+//    }
+    
     HotRankingModel * model = info.object;
     
     // 改变imageView的内容
     [_currentImageView sd_setImageWithURL:model.cover[@"blurred"]];
     
     // 改变titleLabel
-    _titleLabel.text = model.title;
+    _titleString = model.title;
+    
+    _titleThread = [[NSThread alloc] initWithTarget:self selector:@selector(animationLabel) object:nil];
+    
+    [_titleThread start];
     
     // 重新计算line的长度
     CGSize contentSize1 = [self contentSizeOfString:model.title maxWidth:SW-20 font:[UIFont systemFontOfSize:20]];
@@ -189,17 +261,98 @@
     [_line setFrame:CGRectMake(_line.frame.origin.x, _line.frame.origin.y, contentSize1.width*0.8, _line.frame.size.height)];
     
     // 改变第二个label
-    _categryAndTimeLabel.text = [NSString stringWithFormat:@"#%@ / %02ld'%02ld''",model.category,model.duration/60,model.duration%60];
+    _categryAndTimeString = [NSString stringWithFormat:@"#%@ / %02ld'%02ld''",model.category,model.duration/60,model.duration%60];
+    
+    _categryAndTimeThread = [[NSThread alloc] initWithTarget:self selector:@selector(animationLabel1) object:nil];
+    
+    [_categryAndTimeThread start];
+    
     
     // 重新计算第三个label的高度，改变label的内容
-    CGSize contentSize = [self contentSizeOfString:model.myDescription maxWidth:SW-20 font:[UIFont systemFontOfSize:15]];
+    [_descriptionLabel removeFromSuperview];
     
-    [_descriptionLabel setFrame:CGRectMake(_descriptionLabel.frame.origin.x, _descriptionLabel.frame.origin.y, _descriptionLabel.frame.size.width, contentSize.height)];
+    _descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, _categryAndTimeLabel.frame.origin.y + _categryAndTimeLabel.frame.size.height + _rootView.frame.size.height * 0.03, SW - 20, 0)];
     
-    _descriptionLabel.text = model.myDescription;
+    _descriptionLabel.font = [UIFont systemFontOfSize:15];
+    
+    _descriptionLabel.textColor = [UIColor whiteColor];
+    
+    _descriptionLabel.numberOfLines = 0;
+    
+    _descriptionString = model.myDescription;
+    
+    _descriptionThread = [[NSThread alloc] initWithTarget:self selector:@selector(animationLabel3) object:nil];
+    
+    [_descriptionThread start];
+    
+    [_rootView addSubview:_descriptionLabel];
     
     _rootView.alpha = 1;
 }
+
+#pragma mark - 字体动画
+- (void)animationLabel
+{
+    NSString * str = [NSString stringWithFormat:@"%@     ",_titleString];
+    
+    for (NSInteger i = 0; i < str.length; i++)
+    {
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        
+            _titleLabel.text = [str substringWithRange:NSMakeRange(0, i)];
+        }];
+    }
+}
+
+- (void)animationLabel1
+{
+    NSString * str = [NSString stringWithFormat:@"%@     ",_categryAndTimeString];
+    
+    for (NSInteger i = 0; i < str.length; i++)
+    {
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            
+            _categryAndTimeLabel.text = [str substringWithRange:NSMakeRange(0, i)];
+        }];
+    }
+}
+
+- (void)animationLabel2
+{
+    NSString * str = [NSString stringWithFormat:@"%@     ",_descriptionString];
+    
+    for (NSInteger i = 0; i < str.length; i++)
+    {
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            
+            _descriptionLabel.text = [str substringWithRange:NSMakeRange(0, i)];
+            
+            CGSize contentSize = [self contentSizeOfString:_descriptionLabel.text maxWidth:SW-20 font:[UIFont systemFontOfSize:15]];
+            
+            [_descriptionLabel setFrame:CGRectMake(10, _categryAndTimeLabel.frame.origin.y + _categryAndTimeLabel.frame.size.height + _rootView.frame.size.height * 0.03, SW - 20, contentSize.height)];
+        }];
+    }
+}
+
+- (void)animationLabel3
+{
+    NSString * str = [NSString stringWithFormat:@"%@     ",_descriptionString];
+    
+    for (NSInteger i = 0; i < str.length; i++)
+    {
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            
+            _descriptionLabel.text = [str substringWithRange:NSMakeRange(0, i)];
+            
+            NSLog(@"%@",_descriptionLabel.text);
+            
+            CGSize contentSize = [self contentSizeOfString:_descriptionLabel.text maxWidth:SW-20 font:[UIFont systemFontOfSize:15]];
+            
+            [_descriptionLabel setFrame:CGRectMake(10, _categryAndTimeLabel.frame.origin.y + _categryAndTimeLabel.frame.size.height + _rootView.frame.size.height * 0.03, SW - 20, contentSize.height)];
+        }];
+    }
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
